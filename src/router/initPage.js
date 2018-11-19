82a48387daa7cd786df3f73fc8c67e4b4091faa4
sunbaoxu <sunbaoxu@labifenqi.com>
2018-11-19 18:35:06 +0800

    默认首页

--- src/router/initPage.js
+++ src/router/initPage.js
@@ -2,9 +2,9 @@
 // 商家推荐码
 // import ShangCode from '~/shang/shangCode';
 //登陆
-// import ShangCode from '~/login';
+import ShangCode from '~/login';
 //商家列表
-import ShangCode from '~/shang/shangList';
+// import ShangCode from '~/shang/shangList';
 // import ShangCode from '~/shang/shangCode';
 // import ShangCode from '~/shang/shangCode';
 // import ShangCode from '~/shang/shangCode';
